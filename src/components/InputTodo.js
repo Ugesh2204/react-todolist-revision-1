c80935c8a02d8c583463e6acd738bec97646ffc7
ugesh <ugesh.roopchand@hotmail.com>
2022-02-28 20:12:50 +0400

    setState for input field with e.target.value

---
+++ src/components/InputTodo.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = e => {
+    // console.log("hello");
+    this.setState({
+      // title: e.target.value
+      //Name ATTRIBUTE
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Add Todo..." 
+        value={this.state.title}
+        name="title"
+        onChange={this.onChange}
+         />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
