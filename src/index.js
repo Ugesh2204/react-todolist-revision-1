f9fd16f9b7a70b8136e7f2707eb9b3c12cb60a9f
ugesh <ugesh.roopchand@hotmail.com>
2022-03-01 10:11:16 +0400

    CSS STYLE

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
+//stylesheet
+import "./App.css"
 
 // const element = <h1>Hello</h1>
 //component file
-//https://ibaslogic.com/react-tutorial-for-beginners/
+//https://ibaslogic.com/css-in-reactjs-app/
 ReactDOM.render(
 <React.StrictMode>
     <TodoContainer />
